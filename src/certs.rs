785496d0e85fabd25896db64c292447f7d2a2e59
Grant Orndorff <orndorffgrant@protonmail.com>
2020-10-07 23:22:59 -0400

    fix: remove use of unwrap

--- src/certs.rs
+++ src/certs.rs
@@ -1,3 +1,4 @@
+use crate::helpers::stringify;
 use chrono::prelude::*;
 use chrono::Duration;
 use picky::key::PrivateKey;
@@ -18,26 +19,26 @@ const THREE_YEARS: i64 = 365 * 3;
 fn validity_range() -> Result<(UTCDate, UTCDate), String> {
     let today = Local::today();
     let valid_from = UTCDate::ymd(
-        today.year().try_into().unwrap(),
-        today.month().try_into().unwrap(),
-        today.day().try_into().unwrap(),
+        today.year().try_into().map_err(stringify)?,
+        today.month().try_into().map_err(stringify)?,
+        today.day().try_into().map_err(stringify)?,
     )
-    .unwrap();
+    .ok_or("Date construction failed")?;
 
     let three_years_from_now = today + Duration::days(THREE_YEARS);
     let valid_to = UTCDate::ymd(
-        three_years_from_now.year().try_into().unwrap(),
-        three_years_from_now.month().try_into().unwrap(),
-        three_years_from_now.day().try_into().unwrap(),
+        three_years_from_now.year().try_into().map_err(stringify)?,
+        three_years_from_now.month().try_into().map_err(stringify)?,
+        three_years_from_now.day().try_into().map_err(stringify)?,
     )
-    .unwrap();
+    .ok_or("Date construction failed")?;
 
     Ok((valid_from, valid_to))
 }
 
 fn to_writable_bytes(cert: Cert, key: PrivateKey) -> Result<(Vec<u8>, Vec<u8>), String> {
-    let cert_string: String = cert.to_pem().unwrap().into();
-    let key_string: String = key.to_pem().unwrap();
+    let cert_string: String = cert.to_pem().map_err(stringify)?.into();
+    let key_string: String = key.to_pem().map_err(stringify)?;
 
     Ok((
         cert_string.as_bytes().to_vec(),
@@ -47,9 +48,9 @@ fn to_writable_bytes(cert: Cert, key: PrivateKey) -> Result<(Vec<u8>, Vec<u8>), 
 
 pub(crate) fn create_ca() -> Result<(Vec<u8>, Vec<u8>), String> {
     let private_key =
-        PrivateKey::generate_rsa(2048).expect("couldn't extract private key from pkcs8");
+        PrivateKey::generate_rsa(2048).map_err(stringify)?;
 
-    let (valid_from, valid_to) = validity_range().unwrap();
+    let (valid_from, valid_to) = validity_range().map_err(stringify)?;
 
     let ca = CertificateBuilder::new()
         .validity(valid_from, valid_to)
@@ -58,7 +59,7 @@ pub(crate) fn create_ca() -> Result<(Vec<u8>, Vec<u8>), String> {
         .signature_hash_type(SignatureAlgorithm::RsaPkcs1v15(HashAlgorithm::SHA2_512))
         .key_id_gen_method(KeyIdGenMethod::SPKFullDER(HashAlgorithm::SHA2_384))
         .build()
-        .expect("couldn't generate root ca");
+        .map_err(stringify)?;
 
     to_writable_bytes(ca, private_key)
 }
@@ -68,15 +69,15 @@ pub(crate) fn create_cert(
     ca_cert_pem: &[u8],
     ca_pkey_pem: &[u8],
 ) -> Result<(Vec<u8>, Vec<u8>), String> {
-    let ca_pkey_pem_string = String::from_utf8(ca_pkey_pem.to_vec()).unwrap();
-    let ca_key = PrivateKey::from_pem_str(&ca_pkey_pem_string).unwrap();
+    let ca_pkey_pem_string = String::from_utf8(ca_pkey_pem.to_vec()).map_err(stringify)?;
+    let ca_key = PrivateKey::from_pem_str(&ca_pkey_pem_string).map_err(stringify)?;
 
-    let ca_cert_pem_string = String::from_utf8(ca_cert_pem.to_vec()).unwrap();
-    let ca_cert = Cert::from_pem_str(&ca_cert_pem_string).unwrap();
+    let ca_cert_pem_string = String::from_utf8(ca_cert_pem.to_vec()).map_err(stringify)?;
+    let ca_cert = Cert::from_pem_str(&ca_cert_pem_string).map_err(stringify)?;
 
-    let cert_key = PrivateKey::generate_rsa(2048).expect("couldn't extract private key from pkcs8");
+    let cert_key = PrivateKey::generate_rsa(2048).map_err(stringify)?;
 
-    let (valid_from, valid_to) = validity_range().unwrap();
+    let (valid_from, valid_to) = validity_range().map_err(stringify)?;
 
     let cert = CertificateBuilder::new()
         .validity(valid_from, valid_to)
@@ -88,7 +89,7 @@ pub(crate) fn create_cert(
         .key_id_gen_method(KeyIdGenMethod::SPKFullDER(HashAlgorithm::SHA2_384))
         .issuer_cert(&ca_cert, &ca_key)
         .build()
-        .expect("couldn't generate leaf cert");
+        .map_err(stringify)?;
 
     to_writable_bytes(cert, cert_key)
 }
